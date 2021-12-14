771e393d4ec1508701d039bfcfbc83edd913bc89
Daniil Laureanti <daniil.laureanti@gmail.com>
2021-12-14 19:50:04 +0200

    ver 1.1 Add DatePickerFragment, dialog_date.xml

--- app/src/main/java/com/laureanti/daniil/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/laureanti/daniil/android/criminalintent/CrimeFragment.java
@@ -14,11 +14,13 @@ import android.widget.EditText;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import java.util.UUID;
 
 public class CrimeFragment extends Fragment {
     private static final String ARG_CRIME_ID = "crime_id";
+    private static final String DIALOG_DATE = "DialogDate";
 
     private Crime mCrime;
     private EditText mTitleField;
@@ -50,7 +52,13 @@ public class CrimeFragment extends Fragment {
         mDateButton = (Button) v.findViewById(R.id.crime_date);
 //        mDateButton.setText(mCrime.getDate().toString());
         mDateButton.setText(mCrime.getCurrentDate());
-        mDateButton.setEnabled(false);
+        mDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager manager = getFragmentManager();
+                DatePickerFragment dialog = new DatePickerFragment();
+                dialog.show(manager, DIALOG_DATE);
+            } });
 
         mTitleField = (EditText) v.findViewById(R.id.crime_title);
         mTitleField.setText(mCrime.getTitle());
---
+++ app/src/main/java/com/laureanti/daniil/android/criminalintent/DatePickerFragment.java
@@ -0,0 +1,25 @@
+package com.laureanti.daniil.android.criminalintent;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
+
+public class DatePickerFragment extends DialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        View v = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_date, null);
+
+        return new AlertDialog.Builder(getActivity())
+                .setView(v)
+                .setTitle(R.string.date_picker_title)
+                .setPositiveButton(android.R.string.ok, null)
+                .create();
+    }
+}
